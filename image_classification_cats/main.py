c68794e7916d9d2dbb5de747d4ca1f36ef1a743b
Maturrin <migue.solinas@gmail.com>
2018-04-22 15:17:01 +0200

    week2

--- image_classification_cats/main.py
+++ image_classification_cats/main.py
@@ -1,24 +1,18 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from  image_classification_cats.functions import *
-
 from image_classification_cats.lr_utils import load_dataset
-
-# Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
-# Example of a picture
+
 index = 10
 plt.imshow(train_set_x_orig[index])
-# plt.show()
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
-### START CODE HERE ### (≈ 3 lines of code)
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
 m_train =train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = test_set_x_orig.shape[1]
-### END CODE HERE ###
+
 
 print ("Number of training examples: m_train = " + str(m_train))
 print ("Number of testing examples: m_test = " + str(m_test))
@@ -30,12 +24,9 @@ print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 
 
-# Reshape the training and test examples
-
-### START CODE HERE ### (≈ 2 lines of code)
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],num_px*num_px*train_set_x_orig.shape[3]).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],num_px*num_px*test_set_x_orig.shape[3]).T
-### END CODE HERE ###
+
 
 print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
 print ("train_set_y shape: " + str(train_set_y.shape))
