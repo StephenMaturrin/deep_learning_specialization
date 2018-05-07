49d6ca527a17f7644981e2dfaf2d8f6a8ec7fb1f
Maturrin <migue.solinas@gmail.com>
2018-05-07 23:35:24 +0200

    week2

---
+++ image_classification_deep_NN/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from image_classification_deep_NN.testCases_v4 import *
+from image_classification_deep_NN.dnn_utils_v2 import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+plt.show()
+
+
+
+
+np.random.seed(1)
